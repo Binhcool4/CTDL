097c71dedd1e1bef24de847ea47ef81eaaf10329
Binhcool4 <binhtrieuhn99@gmail.com>
2025-06-16 20:54:31 +0700

    Delete Btvn3-Session1.c

--- Btvn3-Session1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int* mallocArray(int n) {
-    int* arr = (int*)malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++) {
-        arr[i] = i;
-    }
-    return arr;
-}
-//Độ phức tạp không gian: O(n)
